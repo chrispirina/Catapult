c15f1e055f8b4b30f2612288350a67d79d345262
chrispirina <33334781+chrispirina@users.noreply.github.com>
2018-11-14 15:46:23 +1100

    rotates

---
+++ Catapult/Assets/Scripts/Arm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : MonoBehaviour {
+
+    public float rotateSpd = 5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Rotate(0, 0 , rotateSpd);
+        if (Input.GetKey(KeyCode.S))
+            transform.Rotate(0, 0, -rotateSpd);
+
+    }
+}
---
+++ Catapult/Assets/Scripts/Catapult.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catapult : MonoBehaviour
+{
+    public float rotateSpd = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0, rotateSpd, 0);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(0, -rotateSpd, 0);
+
+    }
+}
